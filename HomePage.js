977189428dd6a131ef6a918b45dd286aefd2cbf2
AbdullahAlnatour <144846375+AbdullahAlnatour@users.noreply.github.com>
2023-10-07 01:08:49 +0300

    Create HomePage.js

---
+++ HomePage.js
@@ -0,0 +1,42 @@
+function HomePage(){
+    return (<main className="header">    
+
+    <section>
+        <article>
+            <img src="img/img1.PNG" alt="Promotional banner" width={1250} height={200} className="border"/>
+        </article>
+    </section>
+    <br/>
+    <section className="color">
+        <article className="b">
+            <h2>Kebab</h2>
+            <img src="img/img2.PNG" alt="kebab photo" width={400} height={300}/>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. It was popularised in</p>
+        </article>
+        <article className="b">
+            <h2>Salad</h2>
+            <img src="img/img3.PNG" alt="salad photo" width={400} height={300}/>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. It was popularised in</p>
+        </article>
+        <article className="b">
+            <h2>Appetizers</h2>
+            <img src="img/img5.jpg" alt="appetizers" width={400} height={300}/>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. It was popularised in</p>
+        </article>
+    </section><br></br></main>
+    );
+}
+
+export default HomePage;
