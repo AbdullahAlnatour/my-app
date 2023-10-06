af7024c969ef0c7381600e80fb705b9a3259ac83
AbdullahAlnatour <144846375+AbdullahAlnatour@users.noreply.github.com>
2023-10-07 01:07:34 +0300

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return( <footer>
+    <div><img src="img/Logo little lemon.PNG" alt="copy right logo" width={70} height={70}/></div>
+    <div class="r">&copy; copyright little lemon</div>
+</footer>);
+}
+
+export default Footer;
