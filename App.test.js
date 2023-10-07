1e783e28f3034c40a936e4bb3e5e448225a10bab
AbdullahAlnatour <144846375+AbdullahAlnatour@users.noreply.github.com>
2023-10-07 12:43:37 +0300

    Update App.test.js

--- App.test.js
+++ App.test.js
@@ -2,17 +2,20 @@ import { fireEvent,render, screen } from '@testing-library/react';
 import BookingPage from './BookingPage';
 const { default: TestRunner } = require("jest-runner");
 test('Renders the booking form heading', () => {
-  render(<BookingPage />);
+  const handleSubmit=jest.fn();
+  render(<BookingPage onSubmit={handleSubmit}/>);
   const headingElement = screen.getByText("BookingForm");
   expect(headingElement).toBeInTheDocument();
 
-  const sumbit=screen.getByRole("button");
-  expect(sumbit).toBeVisible();
+  const submit=screen.getByRole("button");
+  expect(submit).toBeVisible();
 
   const calendar=screen.getByLabelText(/Choose date/);
   fireEvent.change(calendar, {target:{value:"10/7/2023"}})
 
   const number=screen.getByLabelText(/Number of guests/);
   fireEvent.change(number,{target:{value:"3"}});
+
+  expect(handleSubmit).not.toHaveBeenCalled();
 
 });
